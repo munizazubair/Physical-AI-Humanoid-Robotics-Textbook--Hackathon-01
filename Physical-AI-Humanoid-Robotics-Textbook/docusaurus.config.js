1a2a477af5a970e40dbd6e55c38087a8e71136e0
munizazubair <munizazubairkhan@gmail.com>
2025-12-18 04:15:11 +0500

    update docusaurus.config.js

--- Physical-AI-Humanoid-Robotics-Textbook/docusaurus.config.js
+++ Physical-AI-Humanoid-Robotics-Textbook/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SIBGHAT', // Usually your GitHub org/user name.
+  organizationName: 'munizazubair', // Usually your GitHub org/user name.
   projectName: 'Physical-AI-Humanoid-Robotics-Textbook', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
