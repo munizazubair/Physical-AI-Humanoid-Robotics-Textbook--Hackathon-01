38317f1cea653df2ef1cbb2c168c441054cc8649
munizazubair <munizazubairkhan@gmail.com>
2025-12-18 04:38:47 +0500

    Humanoid

--- Physical-AI-Humanoid-Robotics-Textbook/docusaurus.config.js
+++ Physical-AI-Humanoid-Robotics-Textbook/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://SIBGHAT.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics-Textbook/',
+  baseUrl: 'Physical-AI-Humanoid-Robotics-Textbook--Hackathon-01/Physical-AI-Humanoid-Robotics-Textbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
